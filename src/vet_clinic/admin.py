8fdc0cb425be5826ea86ef5240ea4d7b5c1fd121
Andrewakiv <danchyshy.andriy@gmail.com>
2024-01-09 23:54:36 +0200

    model for services

--- src/vet_clinic/admin.py
+++ src/vet_clinic/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Service
 
-# Register your models here.
+
+@admin.register(Service)
+class PtsAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'photo', 'description', 'publish_date', 'updated_date', 'is_published']
+    list_display_links = ['id', 'title']
+    prepopulated_fields = {'slug': ('title', )}
+    ordering = ['-price', 'title']
+    # actions = ['set_to_published', 'set_to_draft']
+    search_fields = ['is_published', 'title']
+    # list_filter = [PassportFilter, 'is_published', 'category__name']
+    # readonly_fields = ['post_photo']
--- src/vet_clinic/models.py
+++ src/vet_clinic/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+
+class Service(models.Model):
+    title = models.CharField(max_length=50)
+    slug = models.SlugField(max_length=50, unique=True, db_index=True)
+    description = models.TextField(blank=True)
+    content = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    photo = models.ImageField(upload_to='services/%Y/%m/%d/', blank=True, null=True, default=None)
+    publish_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+    is_published = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Service'
+        verbose_name_plural = 'Services'
+        ordering = ['-price']
+        indexes = [
+            models.Index(fields=['-price']),
+        ]
+
+    # def get_absolute_url(self):
+    #     return reverse("pts:post_view", kwargs={"post_slug": self.slug})
